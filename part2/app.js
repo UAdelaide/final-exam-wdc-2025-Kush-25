968121183ca9bdbafcd79cc89155fc0295897a3e
WDC Student (missing default git user name) <kushgupta2005@gmail.com>
2025-06-20 02:49:49 +0000

    [automatic commit] 2025-06-20 02:49:49 UTC 60.225.212.42

--- part2/app.js
+++ part2/app.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-const session = require('express-session'); // <-- ADD THIS LINE
-
+const session = require('express-session');
 const app = express();
 
-// <-- ADD THIS BLOCK START -->
 app.use(session({
     secret: process.env.SESSION_SECRET || 'a_default_secret_please_change_this_in_env', // IMPORTANT: CHANGE THIS IN YOUR .ENV
     resave: false,
     saveUninitialized: false,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 } // Session lasts 24 hours
+    cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24hr
 }));
-// <-- ADD THIS BLOCK END -->
 
 // Middleware
 app.use(express.json());
