660143ecf7dbcea4e58410a144536f91fa13edd2
WDC Student (missing default git user name) <kushgupta2005@gmail.com>
2025-06-21 04:53:15 +0000

    [automatic commit] 2025-06-21 04:53:14 UTC 60.225.212.42

--- part1/app.js
+++ part1/app.js
@@ -23,7 +23,7 @@ async function main() {
       VALUES
         ('alice123', 'alice@example.com', 'hashed123', 'owner'),
         ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-        ('carol123', 'carol@example.com', 'hashed789', 'owner')
+        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
         ('newwalker', 'new@example.com', 'hashednew', 'walker')
     `);
 
